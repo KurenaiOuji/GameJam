50244c20d03753cc03816d5a110701528def995d
SamGtzs <samuelgtz025@gmail.com>
2023-09-22 20:10:13 -0600

    idle y run de caperucita, y arregle el escenario

--- Assets/scripts/Movimiento.cs
+++ Assets/scripts/Movimiento.cs
@@ -1,20 +1,55 @@
+using System;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
     public float movementSpeed = 5f;
+    
 
     private Rigidbody2D rb;
+    private Animator animator;
+    private bool mirandoDerecha = true;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
     }
 
-    private void Update()
+  
+     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(horizontalInput * movementSpeed, rb.velocity.y);
+        
+        procesarMovimiento();
+       
+    }
+
+    void procesarMovimiento()
+    {
+        float inputMovimiento = Input.GetAxis("Horizontal");
+
+        if (inputMovimiento != 0f)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        Vector2 movement = new Vector2(inputMovimiento * movementSpeed, rb.velocity.y);
         rb.velocity = movement;
+
+        gestionarOrientacion(inputMovimiento);
+    }
+    void gestionarOrientacion(float inputMovimiento)
+    {
+        if((mirandoDerecha == true && inputMovimiento < 0) || (mirandoDerecha == false && inputMovimiento > 0))
+        {
+            mirandoDerecha = !mirandoDerecha;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
     }
 }
