82e29b5eb56b543962fc13ec3852983c85cd5de6
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2023-09-25 10:28:44 -0600

    SceneManager Fix
    SceneManager Fix

--- Assets/scripts/Scene.cs
+++ Assets/scripts/Scene.cs
@@ -4,18 +4,23 @@ using UnityEngine.SceneManagement;
 public class Scene : MonoBehaviour
 {
     Score score;
+    [SerializeField] int ScoreMax;
     
     void Start()
     {
         score = FindFirstObjectByType<Score>();
     }
 
+    private void Update()
+    {
+        LoadScene();
+    }
+
     public void LoadScene()
     {
-        if (score.puntuacion >= 150)
+        if (score.puntuacion >= ScoreMax)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
         }
     }
 }
