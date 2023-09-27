682ea935fb2942cbed6b0cba926eb6bcce7ddf1b
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2023-09-27 08:19:16 -0600

    Cronometro
    Cronometro funcional

---
+++ Assets/scripts/Cronometro.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cronometro : MonoBehaviour
+{
+    [SerializeField] float Tiempo;
+    [SerializeField] int Scene;
+    [SerializeField] int MaxScore;
+    Score score;
+
+    void Start()
+    {
+        score = FindFirstObjectByType<Score>();
+    }
+
+    void Update()
+    {
+        cronometro();
+    }
+
+    public void cronometro()
+    {
+        Tiempo -= Time.deltaTime;
+        if(Tiempo <= 0 && score.puntuacion <= MaxScore)
+        {
+            SceneManager.LoadScene(Scene);
+        }
+    }
+}
